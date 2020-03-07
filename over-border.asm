56db2b5f91309f8f923e04afca70c04f5bce6e61
heras <joepgeevers@gmail.com>
2020-03-07 15:08:49 +0100

    two interrupts at right locations

--- over-border.asm
+++ over-border.asm
@@ -5,32 +5,86 @@
 interrupt
     sei         ; ignore interrupts
 
+    ; disable all interrupts and clear pending interrupts
     lda #%0111111    
     sta $dc0d ; interrupt control register CIA #1
     sta $dd0d ; interrupt control register CIA#2
 
-   lda $dc0d   ;clear any pending interrupts CIA#1
-   lda $dd0d    ; clear any pending interrupts CIA#2
+   lda $dc0d   ;clear any pending interrupts CIA#1 by reading
+   lda $dd0d    ; clear any pending interrupts CIA#2 by reading
 
-   lda #$01   ;turn on the raster interrupt
+    ; enable raster interrupt
+   lda #%00000001   ;turn on the raster interrupt
    sta $d01a  ; irq mask register
 
-   ldy #$aa   ;generate interrupt on first line on main screen area
+    ; setting the interrupt line
+   ldy #$33   ;generate interrupt on first line on main screen area
    sty $d012  ; write: line to compare for raster interrupt
-
    lda #$1b  ;clear high order bit of the raster interrupt compare. very naive
    sta $d011
 
-   lda #<irq  ;set the correct address for the interrupt
+    ; set the interrupt vector
+   lda #<white  ;set the correct address for the interrupt
    sta $0314
-   lda #>irq
+   lda #>white
    sta $0315
 
    cli      ; enable interrupts 
-
     rts
 
-irq
+white
    asl $d019 ; Acknowledge interrupt by clearing VICs interrupt flag
-   inc $d020 ; Change border colour
-   jmp $ea81 ; Kernel routine
+    lda #$01
+    sta $d021 ; Change border colour
+
+    ; setting the interrupt line
+   ldy #$f9   ;generate interrupt on first line on main screen area
+   sty $d012  ; write: line to compare for raster interrupt
+   lda #$1b  ;clear high order bit of the raster interrupt compare. very naive
+   sta $d011
+
+    ; set the interrupt vector
+   lda #<black  ;set the correct address for the interrupt
+   sta $0314
+   lda #>black
+   sta $0315
+
+
+   jmp $ea31 ; Kernel routine but skip scanning the keyboard
+
+
+black
+   asl $d019 ; Acknowledge interrupt by clearing VICs interrupt flag
+    lda #$00
+    sta $d021 ; Change border colour
+
+    ; setting the interrupt line
+   ldy #$33   ;generate interrupt on first line on main screen area
+   sty $d012  ; write: line to compare for raster interrupt
+   lda #$1b  ;clear high order bit of the raster interrupt compare. very naive
+   sta $d011
+
+    ; set the interrupt vector
+   lda #<white  ;set the correct address for the interrupt
+   sta $0314
+   lda #>white
+   sta $0315
+
+   jmp $ea31 ; Kernel routine but skip scanning the keyboard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
